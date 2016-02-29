9ae28d2f0f8d9dfa0209ef253833d73e28eb832d
JordanPeltier <peltier.jordan@gmail.com>
2016-02-29 22:18:54 +0100

    Commit3

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QPushButton>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    //MainWindow w;
+    QPushButton bouton("Salut les Zéros, la forme ?");
+    bouton.setText("Pimp mon bouton !");
+
+    bouton.show();
+
+    //w.show();
 
     return a.exec();
 }
